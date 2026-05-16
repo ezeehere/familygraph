59bcd8d62f37e0e5940b5afb9ee49eca95312de8
ezeehere <ezazstudy@.com>
2026-05-16 19:41:39 +0530

    Remove duplicate old request function

--- js/utils.js
+++ js/utils.js
@@ -132,25 +132,6 @@ async checkBackendHealth() {
   }
 },
 
-  async request(path, options = {}) {
-    const response = await fetch(`${API_BASE_URL}${path}`, {
-      ...options,
-      headers: {
-        "Content-Type": "application/json",
-        ...(options.headers || {})
-      }
-    });
-
-    const data = await response.json().catch(() => {
-      return {};
-    });
-
-    if (!response.ok) {
-      throw new Error(data.message || "Request failed.");
-    }
-
-    return data;
-  },
 
   async loadData() {
   try {
